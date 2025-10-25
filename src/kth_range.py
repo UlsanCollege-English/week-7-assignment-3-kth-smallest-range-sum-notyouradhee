e47d76ea5ac377335bbd889adba8933e13b3ce97
notyouradhee <guptaadit2004@gmail.com>
2025-10-25 12:41:48 +0545

    kth range passed

--- src/kth_range.py
+++ src/kth_range.py
@@ -6,12 +6,60 @@ class Node:
 
 def insert(root, key):
     # TODO: return new root after insert (reject duplicates)
-    pass
+    if root is None:
+        return Node(key)
+
+    node = root
+    parent = None
+    while node is not None:
+        parent = node
+        if key < node.key:
+            node = node.left
+        elif key > node.key:
+            node = node.right
+        else:
+            # duplicate, reject
+            return root
+
+    if key < parent.key:
+        parent.left = Node(key)
+    else:
+        parent.right = Node(key)
+
+    return root
 
 def kth_smallest(root, k):
     # TODO
-    pass
+    if k < 1:
+        raise IndexError('k is out of bounds')
+
+    stack = []
+    node = root
+    count = 0
+
+    while stack or node:
+        while node is not None:
+            stack.append(node)
+            node = node.left
+
+        node = stack.pop()
+        count += 1
+        if count == k:
+            return node.key
+
+        node = node.right
+
+    raise IndexError('k is out of bounds')
 
 def range_sum_bst(root, low, high):
     # TODO
-    pass
+    if root is None:
+        return 0
+
+    if root.key < low:
+        return range_sum_bst(root.right, low, high)
+
+    if root.key > high:
+        return range_sum_bst(root.left, low, high)
+
+    return root.key + range_sum_bst(root.left, low, high) + range_sum_bst(root.right, low, high)
